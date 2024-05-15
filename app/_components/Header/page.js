cddd6e18795df26f746c40aa5c2fdbffda3793ce
Mahak Bharatkar <mahakbharatkar@Mahaks-MacBook-Air.local>
2024-05-16 01:11:14 +0530

    header component

---
+++ app/_components/Header/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = ({ darkMode, setDarkMode }) => {
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className="h-[80px] flex justify-between bg-white dark:bg-slate-800">
+      <div className="text-[24px] font-[800] text-left leading-[32.74px] ml-20 mt-4 dark:text-white">
+        Where in the world?
+      </div>
+      <button
+        className={`text-[12px] font-[600] h-10 mt-4 mr-20 ${
+          darkMode ? "text-white" : "text-black"
+        }`}
+        onClick={toggleMode}
+      >
+        {darkMode ? "Dark mode" : "Light mode"}
+      </button>
+    </div>
+  );
+};
+
+export default Header;
